312f57617ce2844ac124e4714afbdd5dcdce8c0e
Raphael César Queiroz do Nascimento <queiroz.raphael@academico.ifrn.edu.br>
2023-07-04 14:31:15 -0300

    enemy 2 and enemy test

---
+++ Underground/Assets/Scripts/Enemy2v.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2v : MonoBehaviour
+{
+    public Rigidbody2D rig;
+
+    public float speed;
+
+    public bool movingRight = true;
+    public float distance;
+
+    public Transform wallDetection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(wallDetection.position, Vector2.right, distance);
+
+        if (groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        } 
+    }
+}
