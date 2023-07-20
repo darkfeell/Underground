c733670cde090162aa387fb3cf384b53ff67d197
raphael <raphaelqueiroz775@gmail.com>
2023-07-20 00:13:41 -0300

    movement and jumping animations and some general optimizations

--- Underground/Assets/Scripts/player.cs
+++ Underground/Assets/Scripts/player.cs
@@ -8,14 +8,10 @@ public class player : MonoBehaviour
 {
     public Rigidbody2D rig;
     public int speed;
-    public bool isGrounded;
+    public bool isJumping;
     public int jumpForce;
     public Animator anim;
     public SpriteRenderer sprt;
-    //public GameObject attackPoint;
-    //public float rad;
-    //public LayerMask enemies;
-    //public float inputX;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,12 +21,9 @@ public class player : MonoBehaviour
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        isGrounded = true;
-    }
-
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        isGrounded = false;
+        if(collision.collider.CompareTag("Ground")){
+            isJumping = false;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -45,58 +38,42 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+        Jump();
+    }
+
+    void Move(){
         float inputX = Input.GetAxisRaw("Horizontal");
-        float inputY = Input.GetAxisRaw("Vertical");
 
-        Vector3 inputTeclado = new Vector3(inputX, 0, 0);
-        transform.position += inputTeclado * speed * Time.deltaTime;
+        rig.velocity = new Vector2(inputX * speed, rig.velocity.y);
 
-        
-
-        //Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
-
-        //movement *= Time.deltaTime;
-        
-        //transform.Translate((movement));
-
-        //rig.velocity = new Vector2(speed * inputX, rig.velocity.y);
-
-        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
-        {
-            GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            anim.SetBool("isJumping", true);
-            anim.SetBool("isWalking", false);
-            anim.SetBool("isIdle", false);
-        }
-        if(Input.GetKeyDown(KeyCode.D) && isGrounded){
-            anim.SetBool("isJumping", false);
-            anim.SetBool("isWalking", true);
-            anim.SetBool("isIdle", false);
+        if(inputX > 0){
             sprt.flipX = false;
-        }
-        if(Input.GetKeyDown(KeyCode.A) && isGrounded){
-            anim.SetBool("isJumping", false);
-            anim.SetBool("isWalking", true);
-            anim.SetBool("isIdle", false);
+            if(!isJumping){
+                anim.SetInteger("transition", 1);
+            }
+            
+        } 
+        else if(inputX < 0){
+            if(!isJumping){
+               anim.SetInteger("transition", 1); 
+            }
             sprt.flipX = true;
         }
-        if(Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.Space) && isGrounded == true){
-            anim.SetBool("isJumping", false);
-            anim.SetBool("isWalking", false);
-            anim.SetBool("isIdle", true);
+
+        if(inputX == 0 && !isJumping){
+            anim.SetInteger("transition", 0);
         }
-
-
-
     }
-    //public void attack(){
-            //Collider2D[] enemy = Physics2D.OverlapCircleAll(attackPoint.transform.position, rad, enemies);
-
-            //foreach(Collider2D enemyGameobject in enemies){
-
-            //}
-        //}
-    //private void OnDrawGizmos(){
-        //Gizmos.DrawWireSphere
-    //}
+    void Jump(){
+       if(Input.GetKey(KeyCode.Space)){
+        if (!isJumping)
+        {
+            anim.SetInteger("transition", 2);
+            rig.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isJumping = true;
+        }
+       } 
+    }
+    
 }
